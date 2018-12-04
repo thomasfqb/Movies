ce1f1d1f301faf5c25e96f3fe2189fa12836999b
Thomas Fauquemberg <thomas.fauquemberg@utt.fr>
2018-12-04 17:02:11 -0500

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Thomas Fauquemberg on 04/12/2018.
+//  Copyright © 2018 Thomas Fauquemberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
